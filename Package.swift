8b9f2e883aacd1726f5517762678eb28a3c2f32e
Joel Spadin <joelspadin@gmail.com>
2024-10-19 12:31:40 -0500

    Regenerate the rest of the binding files
    For real this time

--- Package.swift
+++ Package.swift
@@ -3,46 +3,35 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterDevicetree",
-    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterDevicetree", targets: ["TreeSitterDevicetree"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterDevicetree",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    // NOTE: if your language has an external scanner, add it here.
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterDevicetree",
+            dependencies: [],
+            path: ".",
+            sources: [
+                "src/parser.c",
+                // NOTE: if your language has an external scanner, add it here.
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterDevicetreeTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterDevicetree",
+            ],
+            path: "bindings/swift/TreeSitterDevicetreeTests"
+        )
     ],
     cLanguageStandard: .c11
 )
